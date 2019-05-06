4905c484d700c54c3b0ea10dada921d7b4b7af56
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2019-05-06 21:30:04 +0100

    added code to load user notifications in GUI

--- src/application/NotificationViewController.java
+++ src/application/NotificationViewController.java
@@ -1,7 +1,11 @@
 package application;
 
+import java.util.List;
+
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
+import model.Notification;
+import model.User;
 
 public class NotificationViewController {
     
@@ -13,6 +17,14 @@ public class NotificationViewController {
     
     @FXML
     public void initialize() {
+        List<Notification> userNotifications = ((User)ScreenManager.getCurrentUser()).getNotifications();
         
+        for(Notification notification: userNotifications) {
+            if(notification.isRead()) {
+                pastNotificationsBox.getChildren().add(notification.getNotificationBox());
+            } else {
+                newNotificationsBox.getChildren().add(notification.getNotificationBox());
+            }
+        }
     }
 }
