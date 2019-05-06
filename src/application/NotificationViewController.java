f065715d4b87aab04b33d16016e8adf864f30601
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2019-05-06 20:58:03 +0100

    added notification view controller

---
+++ src/application/NotificationViewController.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+public class NotificationViewController {
+    
+    @FXML
+    private VBox newNotificationsBox;
+    
+    @FXML
+    private VBox pastNotificationsBox;
+    
+    @FXML
+    public void initialize() {
+        
+    }
+}
