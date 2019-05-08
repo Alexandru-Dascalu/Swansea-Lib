6fb7b205df5556f800c0e9cbb31d2fe8600f6e21
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2019-05-08 10:42:31 +0100

    added code to mark all notifications as read

--- src/application/NotificationViewController.java
+++ src/application/NotificationViewController.java
@@ -1,11 +1,15 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import model.DBHelper;
 import model.Notification;
 import model.User;
 
@@ -25,7 +29,8 @@ public class NotificationViewController {
     
     @FXML
     public void initialize() {
-        List<Notification> userNotifications = ((User)ScreenManager.getCurrentUser()).getNotifications();
+        List<Notification> userNotifications = ((User)ScreenManager.
+                getCurrentUser()).getNotifications();
         
         for(Notification notification: userNotifications) {
             if(notification.isRead()) {
@@ -36,5 +41,38 @@ public class NotificationViewController {
         }
         
         System.out.println("vbox "+newNotificationsBox.getPrefWidth());
+    }
+    
+    @FXML
+    private void markAllAsRead() {
+        User currentUser = (User) ScreenManager.getCurrentUser();
+        List<Notification> userNotifications = currentUser.getNotifications();
+        
+        for(Notification notif: userNotifications) {
+            if(!notif.isRead()) {
+                notif.setRead();
+            }
+        }
+        
+        try (Connection dbConnection = DBHelper.getConnection();
+                PreparedStatement updateStatement = dbConnection.prepareStatement(
+                "UPDATE userNotifications SET seen = ? WHERE username = ? AND seen" +
+                " = ?")) {
+            
+            updateStatement.setBoolean(1, true);
+            updateStatement.setString(2, currentUser.getUsername());
+            updateStatement.setBoolean(3, false);
+            
+            updateStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            AlertBox.showErrorAlert("Because the SQLite database library we use " +
+                    " for this program, notifications for this user" +
+                    " could not be loaded (database locks up for no reason, says it is" +
+                    " busy). Close the program and restart it to see your notifications.");
+        }
+        
+        pastNotificationsBox.getChildren().addAll(newNotificationsBox.getChildren());
+        newNotificationsBox.getChildren().clear();
     }
 }
