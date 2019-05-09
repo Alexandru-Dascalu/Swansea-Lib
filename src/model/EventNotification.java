e03f2007c20202861e0d1dd5556abf53948e6888
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2019-05-09 20:44:38 +0100

    more coding convetions

--- src/model/EventNotification.java
+++ src/model/EventNotification.java
@@ -19,110 +19,130 @@ import javafx.scene.text.Text;
  * @author Alexandru Dascalu
  */
 public class EventNotification extends Notification {
-	
-    /**The date of the event for which this notification is made.*/
-	private final String date;
-	
-	/**Makes the message that should be displayed for a notification when the 
-	 * given event has just been added.
-	 * @param event The new event for which this message is made.
-	 * @return a message for a new event notification.
-	 */
-	public static String getNewEventMsg(Event event) {
-		return "A new event has been added since your last login: " 
-				+ event.getTitle() + ".";
-	}
-	
-	/**
-	 * Makes the message that should be displayed for a notification when the 
-	 * given event will happen in the near future.
-	 * @param event the event that happens in the near future.
-	 * @return the message for a nearing event notification.
-	 */
-	public static String getNearingEventMsg(Event event) {
-		int days = event.getDaysUntilEvent();
-		if(days > 0) {
-			return "You have one event that will be held in " + days + " days: "
-					+ event.getTitle() + ".";
-		} else {
-			return "You have one event that will be in less than a day: " 
-					+ event.getTitle() + ".";
-		}
-	}
-	
-	/**
-	 * Makes a new notification in the database for a newly made event. It also
-	 * associates in the database the newly made notification with users with
-	 * the new event notification setting turned on.
-	 * @param event the new event for which a notification is made.
-	 */
-	public static void makeNewEventNotification(Event event) {
-	    int notificationID;
-	    try (Connection dbConnection = DBHelper.getConnection();
-	            PreparedStatement insertStatement = dbConnection.prepareStatement(
-	            "INSERT INTO notification (message, date) VALUES (?, ?)")) {
+
+    /** The date of the event for which this notification is made. */
+    private final String date;
+
+    /**
+     * Makes a new EventNotification.
+     * 
+     * @param message The message of the new notification.
+     * @param isRead Whether the notification has been marked read by the user.
+     * @param date The date of the event for which the notification is made.
+     */
+    public EventNotification(String message, boolean isRead, String date) {
+        super(message, isRead);
+        this.date = date;
+    }
+
+    
+    /**
+     * Makes the message that should be displayed for a notification when the given
+     * event has just been added.
+     * 
+     * @param event The new event for which this message is made.
+     * @return a message for a new event notification.
+     */
+    public static String getNewEventMsg(Event event) {
+        return "A new event has been added since your last login: " + event.getTitle() + ".";
+    }
+
+    /**
+     * Makes the message that should be displayed for a notification when the given
+     * event will happen in the near future.
+     * 
+     * @param event the event that happens in the near future.
+     * @return the message for a nearing event notification.
+     */
+    public static String getNearingEventMsg(Event event) {
+        int days = event.getDaysUntilEvent();
+        if (days > 0) {
+            return "You have one event that will be held in " + days + " days: " +
+                event.getTitle() + ".";
+        }
+        else {
+            return "You have one event that will be in less than a day: " + event.getTitle() + ".";
+        }
+    }
+
+    /**
+     * Makes a new notification in the database for a newly made event. It also
+     * associates in the database the newly made notification with users with the
+     * new event notification setting turned on.
+     * 
+     * @param event the new event for which a notification is made.
+     */
+    public static void makeNewEventNotification(Event event) {
+        int notificationID;
+        try (Connection dbConnection = DBHelper.getConnection();
+                PreparedStatement insertStatement = dbConnection.prepareStatement(
+                "INSERT INTO notification (message, date) VALUES (?, ?)")) {
             insertStatement.setString(1, EventNotification.getNewEventMsg(event));
-            
+
             insertStatement.setString(2, event.getDateTime());
             insertStatement.executeUpdate();
-            
-            notificationID = insertStatement.getGeneratedKeys().getInt(1);   
-        } catch (SQLException e) {
+
+            notificationID = insertStatement.getGeneratedKeys().getInt(1);
+        }
+        catch (SQLException e) {
             e.printStackTrace();
             AlertBox.showErrorAlert(e.getMessage());
             return;
         }
-	    
-	    List<String> notificationUsers = getNewEventNotificationUsers();
-	    for (String username : notificationUsers) {
+
+        List<String> notificationUsers = getNewEventNotificationUsers();
+        for (String username : notificationUsers) {
             Notification.makeUserNotification(notificationID, username);
         }
-	}
-	
-	/**
-     * Makes a new notification in the database for an event a user is attending
-     * and that is close to the current date. It also associates in the database
-     * the newly made notification with users with the nearing event 
-     * notification setting turned on.
+    }
+
+    /**
+     * Makes a new notification in the database for an event a user is attending and
+     * that is close to the current date. It also associates in the database the
+     * newly made notification with users with the nearing event notification
+     * setting turned on.
+     * 
      * @param event the near future event for which a notification is made.
      * @return the unique ID of the notification created in the database.
      */
-	public static int makeNearingEventNotification(Event event) {
-	    int notificationID;
-	    
-	    try (Connection dbConnection = DBHelper.getConnection();
-	            PreparedStatement insertStatement = dbConnection.prepareStatement(
-	            "INSERT INTO notification (message, date) VALUES (?, ?)")){
+    public static int makeNearingEventNotification(Event event) {
+        int notificationID;
+
+        try (Connection dbConnection = DBHelper.getConnection();
+                PreparedStatement insertStatement = dbConnection.prepareStatement(
+                "INSERT INTO notification (message, date) VALUES (?, ?)")) {
             insertStatement.setString(1, EventNotification.getNearingEventMsg(event));
-            
+
             insertStatement.setString(2, event.getDateTime());
             insertStatement.executeUpdate();
-            
+
             notificationID = insertStatement.getGeneratedKeys().getInt(1);
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
             AlertBox.showErrorAlert(e.getMessage());
             return -1;
         }
-	    
-	    return notificationID;
-	}
-	
-	/**
-	 * Gets a list of users names from the database of users with
-     * the new event notification setting turned on.
-	 * @return list of users names of users with
-     * the new event notification setting turned on.
-	 */
-	public static ArrayList<String> getNewEventNotificationUsers() {
+
+        return notificationID;
+    }
+
+    /**
+     * Gets a list of users names from the database of users with the new event
+     * notification setting turned on.
+     * 
+     * @return list of users names of users with the new event notification setting
+     *         turned on.
+     */
+    public static ArrayList<String> getNewEventNotificationUsers() {
         ArrayList<String> notificationUsers = new ArrayList<>();
-        
+
         try (Connection dbConnection = DBHelper.getConnection();
                 PreparedStatement insertStatement = dbConnection.prepareStatement(
-                "SELECT userName FROM userSettings WHERE newEventSetting = true");
-                ResultSet usernames = insertStatement.executeQuery()){
-            
-            while(usernames.next()) {
+                    "SELECT userName FROM userSettings WHERE newEventSetting = true");
+                ResultSet usernames = insertStatement.executeQuery()) {
+
+            while (usernames.next()) {
                 notificationUsers.add(usernames.getString(1));
             }
         }
@@ -131,25 +151,26 @@ public class EventNotification extends Notification {
             AlertBox.showErrorAlert(e.getMessage());
             return null;
         }
-     
+
         return notificationUsers;
     }
-	
-	/**
-     * Gets a list of users names from the database of users with
-     * the nearing event notification setting turned on.
-     * @return list of users names of users with
-     * the neaering event notification setting turned on.
+
+    /**
+     * Gets a list of users names from the database of users with the nearing event
+     * notification setting turned on.
+     * 
+     * @return list of users names of users with the neaering event notification
+     *         setting turned on.
      */
-	public static ArrayList<String> getNearingNotificationUsers() {
+    public static ArrayList<String> getNearingNotificationUsers() {
         ArrayList<String> notificationUsers = new ArrayList<>();
-        
+
         try (Connection dbConnection = DBHelper.getConnection();
                 PreparedStatement insertStatement = dbConnection.prepareStatement(
                 "SELECT username FROM users");
                 ResultSet usernames = insertStatement.executeQuery()) {
-            
-            while(usernames.next()) {
+
+            while (usernames.next()) {
                 notificationUsers.add(usernames.getString(1));
             }
         }
@@ -157,62 +178,52 @@ public class EventNotification extends Notification {
             e.printStackTrace();
             AlertBox.showErrorAlert(e.getMessage());
         }
-     
+
         return notificationUsers;
     }
-	
-	/**
-	 * Makes a new EventNotification.
-	 * @param message The message of the new notification.
-     * @param isRead Whether the notification has been marked read by the user.
-	 * @param date The date of the event for which the notification is made.
-	 */
-	public EventNotification(String message, boolean isRead, String date) {
-		super(message, isRead);
-		this.date = date;
-	}
-	
-	/**
-	 * Gets the date of the event associated with this notification.
-	 * @return a string representing the date of the event associated with
-	 * this notification.
-	 */
-	public String getDate() {
-		return date;
-	}
-	
-	/**
-     * Returns a string that represents CSS properties which will be used when
-     *  the notification is displayed. It sets a blue background colour with a linear
-     *  gradient, padding, and a border with a blue colour and width.
-     * @return A CSS string with the style properties used to display this 
-     * notification.
+
+    /**
+     * Gets the date of the event associated with this notification.
+     * 
+     * @return a string representing the date of the event associated with this
+     *         notification.
      */
-	public String getStyle() {
-        return "-fx-background-color: linear-gradient(#1184aa, deepskyblue);\r\n" +
-               "-fx-padding: 8;\r\n" +
-               "-fx-border-color: #054256;\r\n" + 
-               "-fx-border-width: 5;\r\n" +
-               "-fx-border-style: solid;";
+    public String getDate() {
+        return date;
     }
-	
-	/**
-     * Makes a new empty HBox which will be used to display this notification. 
-     * It sets the padding, alignment, CSS style (based on getStyle()), 
-     * spacing and growth policy of the new HBox. It adds the message of the 
-     * notification and the event date to the HBox.
+
+    /**
+     * Returns a string that represents CSS properties which will be used when the
+     * notification is displayed. It sets a blue background colour with a linear
+     * gradient, padding, and a border with a blue colour and width.
+     * 
+     * @return A CSS string with the style properties used to display this
+     *         notification.
+     */
+    public String getStyle() {
+        return "-fx-background-color: linear-gradient(#1184aa, deepskyblue);\r\n" +
+            "-fx-padding: 8;\r\n" + "-fx-border-color: #054256;\r\n" + "-fx-border-width: 5;\r\n" +
+            "-fx-border-style: solid;";
+    }
+
+    /**
+     * Makes a new empty HBox which will be used to display this notification. It
+     * sets the padding, alignment, CSS style (based on getStyle()), spacing and
+     * growth policy of the new HBox. It adds the message of the notification and
+     * the event date to the HBox.
+     * 
      * @return the new HBox to display this notification.
      */
-	public HBox getNotificationBox() {
-	    HBox notificationBox = super.getNotificationBox();
-	    notificationBox.getChildren().add(createSpacer());
-	    notificationBox.getChildren().add(getMessageTextElement());
-        
+    public HBox getNotificationBox() {
+        HBox notificationBox = super.getNotificationBox();
+        notificationBox.getChildren().add(createSpacer());
+        notificationBox.getChildren().add(getMessageTextElement());
+
         Label dateLabel = new Label("Event date:");
         dateLabel.setTextFill(Color.BLACK);
         dateLabel.setStyle("-fx-font-weight: bold;");
         notificationBox.getChildren().add(dateLabel);
-        
+
         notificationBox.getChildren().add(new Text(date));
         return notificationBox;
     }
--- src/model/Notification.java
+++ src/model/Notification.java
@@ -17,42 +17,62 @@ import javafx.scene.text.Text;
 
 /**
  * A class that models a notification that users can have. A base notification
- *  has just a message, and can be read or not.
+ * has just a message, and can be read or not.
+ * 
  * @author Alexandru Dascalu
  */
 public abstract class Notification {
-	
-    /**The height of the image view an image associated with this notification
-     *  has. Its value is {@value}.*/
+
+    /**
+     * The height of the image view an image associated with this notification has.
+     * Its value is {@value}.
+     */
     protected static final int IMAGE_HEIGHT = 200;
-    
-    /**The width of the image view an image associated with this notification
-     *  has. Its value is {@value}.*/
+
+    /**
+     * The width of the image view an image associated with this notification has.
+     * Its value is {@value}.
+     */
     protected static final int IMAGE_WIDTH = 120;
-    
-    /**The wrap width of the text showing the message of the notification in 
-     * the GUI. Its value is {@value}.*/
+
+    /**
+     * The wrap width of the text showing the message of the notification in the
+     * GUI. Its value is {@value}.
+     */
     private static final int TEXT_WRAP_WIDTH = 400;
-    
-    /**The message of this notification.*/
-	protected final String message;
-	
-	/**A flag that shows if this notification has been read by the user.*/
-	private boolean isRead;
-	
-	/**Associates the notification with the given id to the given user in
-	 *  the database.
-	 * @param notificationID The id of the notification that is sent to this 
-	 *  user.
-	 * @param username The name of the user to receive the notification.*/
-	public static void makeUserNotification(int notificationID, String username) {
+
+    /** The message of this notification. */
+    protected final String message;
+
+    /** A flag that shows if this notification has been read by the user. */
+    private boolean isRead;
+
+    /**
+     * Makes a new notification.
+     * 
+     * @param message The message of the new notification.
+     * @param isRead Whether the notification has been marked read by the user.
+     */
+    public Notification(String message, boolean isRead) {
+        this.message = message;
+        this.isRead = isRead;
+    }
+
+    /**
+     * Associates the notification with the given id to the given user in the
+     * database.
+     * 
+     * @param notificationID The id of the notification that is sent to this user.
+     * @param username The name of the user to receive the notification.
+     */
+    public static void makeUserNotification(int notificationID, String username) {
         try (Connection dbConnection = DBHelper.getConnection();
                 PreparedStatement insertStatement = dbConnection.prepareStatement(
-                "INSERT INTO userNotifications VALUES (?, ?, false)")){
-            
+                "INSERT INTO userNotifications VALUES (?, ?, false)")) {
+
             insertStatement.setInt(1, notificationID);
             insertStatement.setString(2, username);
-            
+
             insertStatement.executeUpdate();
         }
         catch (SQLException e) {
@@ -60,159 +80,160 @@ public abstract class Notification {
             AlertBox.showErrorAlert(e.getMessage());
         }
     }
-    
-	/**
-	 * Searches the database for a notification with the given message and date.
-	 *  Notifications are considered to be identical if they have the same
-	 *  message and date. So users do not get repeating notifications, we check
-	 *  if a notification like we need already exists.
-	 * @param message The message of a notification we search for.
-	 * @param date The date of a notification we search for.
-	 * @return The unique ID of an already existing notification that has the 
-	 * given details, or -1 of no such notification exists in the database.
-	 */
+
+    /**
+     * Searches the database for a notification with the given message and date.
+     * Notifications are considered to be identical if they have the same message
+     * and date. So users do not get repeating notifications, we check if a
+     * notification like we need already exists.
+     * 
+     * @param message The message of a notification we search for.
+     * @param date The date of a notification we search for.
+     * @return The unique ID of an already existing notification that has the given
+     *         details, or -1 of no such notification exists in the database.
+     */
     public static int getExistingNotificationID(String message, String date) {
         int notificationId;
-        
+
         try (Connection dbConnection = DBHelper.getConnection();
-                PreparedStatement selectStatement = dbConnection.prepareStatement("" +
-                "SELECT * FROM notification WHERE message = ? AND date = ?")) {
+                PreparedStatement selectStatement = dbConnection.prepareStatement(
+                    "" + "SELECT * FROM notification WHERE message = ? AND date = ?")) {
             selectStatement.setString(1, message);
             selectStatement.setString(2, date);
             ResultSet existingNotification = selectStatement.executeQuery();
-            
-            if(existingNotification.next()) {
-                 notificationId = existingNotification.getInt(1);
-            } else {
+
+            if (existingNotification.next()) {
+                notificationId = existingNotification.getInt(1);
+            }
+            else {
                 notificationId = -1;
             }
-            
+
             existingNotification.close();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
             AlertBox.showErrorAlert(e.getMessage());
             return Integer.MIN_VALUE;
         }
-        
+
         return notificationId;
     }
-    
+
     /**
-     * Checks in the database to see if a notification is already associated
-     *  with a user or not.
+     * Checks in the database to see if a notification is already associated with a
+     * user or not.
+     * 
      * @param notificationID The ID of the notification.
      * @param userName The user name of the user.
-     * @return True if the user is already associated with the notification
-     * in the database, false if not.
+     * @return True if the user is already associated with the notification in the
+     *         database, false if not.
      */
     public static boolean existUserNotification(int notificationID, String userName) {
         boolean alreadyExists;
         try (Connection dbConnection = DBHelper.getConnection();
                 PreparedStatement selectStatement = dbConnection.prepareStatement(
-                "SELECT * FROM userNotifications WHERE nID = ? AND username = ?")){
+                    "SELECT * FROM userNotifications WHERE nID = ? AND username = ?")) {
             selectStatement.setInt(1, notificationID);
             selectStatement.setString(2, userName);
-            
+
             ResultSet existingNotification = selectStatement.executeQuery();
-            alreadyExists =  existingNotification.next();
-            
+            alreadyExists = existingNotification.next();
+
             existingNotification.close();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
             AlertBox.showErrorAlert(e.getMessage());
             return false;
-        } 
-        
+        }
+
         return alreadyExists;
     }
-    
+
     /**
-     * Makes a new notification.
-     * @param message The message of the new notification.
-     * @param isRead Whether the notification has been marked read by the user.
+     * Gets the message of this notification.
+     * 
+     * @return the message of this notification.
      */
-	public Notification(String message, boolean isRead) {
-		this.message = message;
-		this.isRead = isRead;
-	}
-	
-	/**
-	 * Gets the message of this notification.
-	 * @return the message of this notification.
-	 */
-	public String getMessage() {
-		return message;
-	}
-	
-	/**
-	 * Says if this notification has been read or not.
-	 * @return true if the notification has been read, false if not.
-	 */
-	public boolean isRead() {
-	    return isRead;
-	}
-	
-	/**Sets the isRead flag to true if it was false. If not, it throws an 
-	 * exception.
-	 * @throws IllegalArgumentException if this method is called when the 
-	 * notification is already read.*/
-	public void setRead() {
-	    if(isRead) {
-	        throw new IllegalStateException("Called setRead when notification" +
-	            " is already read!");
-	    }
-	    isRead = true;
-	}
-	
-	/**
-	 * Returns a string that represents CSS properties which will be used when
-	 *  the notification is displayed.
-	 * @return A CSS string with the style properties used to display this 
-	 * notification.
-	 */
-	public abstract String getStyle();
-	
-	/**
-	 * Makes a new empty HBox which will be used to display this notification. 
-	 * It sets the padding, alignment, CSS style (based on getStyle()), 
-	 * spacing and growth policy of the new HBox.
-	 * @return the new HBox to display this notification.
-	 */
-	public HBox getNotificationBox() {
-	    HBox notificationBox = new HBox();
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Says if this notification has been read or not.
+     * 
+     * @return true if the notification has been read, false if not.
+     */
+    public boolean isRead() {
+        return isRead;
+    }
+
+    /**
+     * Sets the isRead flag to true if it was false. If not, it throws an exception.
+     * 
+     * @throws IllegalArgumentException if this method is called when the
+     *         notification is already read.
+     */
+    public void setRead() {
+        if (isRead) {
+            throw new IllegalStateException(
+                "Called setRead when notification" + " is already read!");
+        }
+        isRead = true;
+    }
+
+    /**
+     * Returns a string that represents CSS properties which will be used when the
+     * notification is displayed.
+     * 
+     * @return A CSS string with the style properties used to display this
+     *         notification.
+     */
+    public abstract String getStyle();
+
+    /**
+     * Makes a new empty HBox which will be used to display this notification. It
+     * sets the padding, alignment, CSS style (based on getStyle()), spacing and
+     * growth policy of the new HBox.
+     * 
+     * @return the new HBox to display this notification.
+     */
+    public HBox getNotificationBox() {
+        HBox notificationBox = new HBox();
         notificationBox.setSpacing(20);
         notificationBox.setStyle(getStyle());
         notificationBox.setAlignment(Pos.CENTER_LEFT);
         notificationBox.setPadding(new Insets(0, 0, 0, 20));
         VBox.setVgrow(notificationBox, Priority.ALWAYS);
         return notificationBox;
-	}
-	
-	/**
-	 * Gets the an empty Text element which should be used to display the
-	 *  message. It has the wrapping width set to TEXT_WRAP_WIDTH.
-	 * @return an empty Text element with the wrapping width set.
-	 */
-	protected Text getMessageTextElement() {
-	    Text messageText = new Text(message);
-	    messageText.setWrappingWidth(TEXT_WRAP_WIDTH);
-	    
-	    return messageText;
-	}
-	
-	/**
-	 * Makes an empty node that can be used to space out elements in an HBox. 
-	 * Used with event notifications so text is aligned with the other 
-	 * notifications, since event notifications do not display images.
-	 * Its height depends on the height of the ImageViews used to display
-	 * resource images.
-	 * @return an empty node used to space out elements in an HBox.
-	 */
-	protected Node createSpacer() 
-    {
-        final Region spacer =  new Region();
-        
-        spacer.setMinHeight(IMAGE_HEIGHT/2);
+    }
+
+    /**
+     * Gets the an empty Text element which should be used to display the message.
+     * It has the wrapping width set to TEXT_WRAP_WIDTH.
+     * 
+     * @return an empty Text element with the wrapping width set.
+     */
+    protected Text getMessageTextElement() {
+        Text messageText = new Text(message);
+        messageText.setWrappingWidth(TEXT_WRAP_WIDTH);
+
+        return messageText;
+    }
+
+    /**
+     * Makes an empty node that can be used to space out elements in an HBox. Used
+     * with event notifications so text is aligned with the other notifications,
+     * since event notifications do not display images. Its height depends on the
+     * height of the ImageViews used to display resource images.
+     * 
+     * @return an empty node used to space out elements in an HBox.
+     */
+    protected Node createSpacer() {
+        final Region spacer = new Region();
+
+        spacer.setMinHeight(IMAGE_HEIGHT / 2);
         spacer.setMinWidth(IMAGE_WIDTH);
         HBox.setHgrow(spacer, Priority.NEVER);
         return spacer;
